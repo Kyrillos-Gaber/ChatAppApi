99a2812f980e8103260c69c10659aa8245e95147
kyrillosgaber <kyrillosgaber@gmail.com>
2023-10-05 16:19:12 +0300

    Create Private Chat

--- ChatAppApi/Hubs/ChatHub.cs
+++ ChatAppApi/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using chatapi.Services;
 using ChatAppApi.Dtos;
 using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
 
 namespace chatapi.Hubs;
 public sealed class ChatHub : Hub
@@ -42,6 +43,37 @@ public sealed class ChatHub : Hub
     public async Task ReceiveMessage(MessageDto message)
     {
         await Clients.Group("Come2Chat").SendAsync("NewMessage", message);
+    }
+
+    public async Task CreatePrivateChat(MessageDto message)
+    {
+        string privateGroupName = GetPrivateGroupName(message.From, message.To!);
+        await Groups.AddToGroupAsync(Context.ConnectionId, privateGroupName);
+        var toConnectionId = _chatService.GetConnectionIdByUser(message.To!);
+
+        await Clients.Client(toConnectionId).SendAsync("OpenPrivateChat", message);
+    }
+
+    public async Task ReceivePrivateMessage(MessageDto message)
+    {
+        string privateGroupName = GetPrivateGroupName(message.From, message.To!);
+        await Clients.Group(privateGroupName).SendAsync("NewPrivateMessage", message);
+    }
+
+    private string GetPrivateGroupName(string from, string to)
+    {
+        Console.WriteLine(string.CompareOrdinal(from, to));
+        var stringCompare = string.CompareOrdinal(from, to) < 0;
+        return stringCompare ? $"{from}-{to}" : $"{to}-{from}";
+    }
+
+    public async Task RemovePrivateChat(string from, string to)
+    {
+        string privateGroupName = GetPrivateGroupName(from, to);
+        await Clients.Group(privateGroupName).SendAsync("ClosePrivateChat");
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, privateGroupName);
+        string toConnectionId = _chatService.GetConnectionIdByUser(to);
+        await Groups.RemoveFromGroupAsync(toConnectionId, privateGroupName);
     }
 
 }
